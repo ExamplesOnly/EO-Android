a3d424055a3cfec187781be194fc10199b231437
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2021-01-02 00:41:26 +0530

    Implement preference fragment - add settings option
    Signed-off-by: Bishwajyoti Roy <bishwajyoti456@gmail.com>

--- app/src/main/java/com/examplesonly/android/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/examplesonly/android/ui/activity/SettingsActivity.java
@@ -2,9 +2,18 @@ package com.examplesonly.android.ui.activity;
 
 import android.os.Bundle;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.examplesonly.android.R;
 import com.examplesonly.android.account.UserDataProvider;
 import com.examplesonly.android.databinding.ActivitySettingsBinding;
+import com.examplesonly.android.ui.fragment.AddDemandFragment;
+import com.examplesonly.android.ui.fragment.CameraFragment;
+import com.examplesonly.android.ui.settings.MainSettings;
+import com.examplesonly.gallerypicker.view.VideosFragment;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -15,13 +24,27 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = ActivitySettingsBinding.inflate(getLayoutInflater());
-
-        binding.logoutBtn.setOnClickListener(view1 -> {
-            UserDataProvider.getInstance(this).logout();
-            finish();
-        });
-
         View view = binding.getRoot();
         setContentView(view);
+
+        setSupportActionBar(binding.topAppBar);
+
+        binding.topAppBar.setNavigationOnClickListener(v -> onBackPressed());
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.root, new MainSettings(), "MainSettings");
+
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.root);
+        if (fragment instanceof MainSettings) {
+            finish();
+        }
+
+        super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/examplesonly/android/ui/settings/MainSettings.java
@@ -0,0 +1,65 @@
+package com.examplesonly.android.ui.settings;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.examplesonly.android.BuildConfig;
+import com.examplesonly.android.R;
+import com.examplesonly.android.account.UserDataProvider;
+
+public class MainSettings extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings_main, rootKey);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        getPreferenceManager().findPreference("version").setSummary(BuildConfig.VERSION_NAME + " (" + BuildConfig.VERSION_CODE + ")");
+        getPreferenceManager().findPreference("feedback").setOnPreferenceClickListener(preference -> {
+            sendFeedback();
+            return false;
+        });
+
+        getPreferenceManager().findPreference("logout").setOnPreferenceClickListener(preference -> {
+            Logout();
+            return false;
+        });
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+
+    void sendFeedback() {
+        try {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:"));
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"dev@examplesonly.com"});
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback for ExamplesOnly");
+//            intent.putExtra(Intent.EXTRA_TEXT, "Feedback for ExamplesOnly");
+            startActivity(intent);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getActivity(),
+                    "There are no email client found on your device. Kindly email us at dev@examplesonly.com",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    void Logout() {
+        UserDataProvider.getInstance(getContext()).logout();
+        getActivity().finish();
+    }
+}
